ba85076801d521445dba2648cd633ead560baa1e
Sean Harte <scsharte@gmail.com>
2021-03-19 17:01:02 -0400

    clened up page transition

--- src/components/StoreWrapper.tsx
+++ src/components/StoreWrapper.tsx
@@ -3,29 +3,35 @@ import { Root } from '../scripts/store'
 import Home from './Home'
 import GroupPage from './GroupPage'
 import { FormGroup } from '../scripts/formGroup'
-import { Box, Link, Button } from "@chakra-ui/react"
+import { Box, Link, Button, Spacer } from "@chakra-ui/react"
 import { ChevronLeftIcon } from "@chakra-ui/icons"
 import { FieldNode } from '../scripts/fieldNode';
 import { AnimatePresence, motion, MotionStyle } from "framer-motion"
 import FieldPage from './FieldPage'
 
-const pageVariants = {
+const variants = {
+  enter: {
+    x:  1000,
+    opacity: 1
+  },
   initial: {
     opacity: 0,
   },
-  in: {
-    opacity: 1,
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
   },
-  out: {
-    opacity: 0,
-  },
-}
+  exit: {
+    x: -1000,
+    opacity: 1
+  }
+};
 
 const pageTransition = {
-  type: "tween",
-  ease: "anticipate",
-  duration: 0.5
-};
+  x: {  stiffness: 300, damping: 30 },
+  opacity: { duration: 0.2 }
+}
 
 const pageStyle: MotionStyle = {
   position: "absolute",
@@ -68,47 +74,52 @@ const StoreWrapper = (props) => {
 
   return (
   <>
-    <Box as="header" d="flex" justifyContent="space-between">
+    <Box as="header" d="flex" >
       {(page !== 'root') && (
-        <Link onClick={prevPage}>
+        <Link d="flex" alignItems="center" onClick={prevPage}>
           <ChevronLeftIcon mx="2px" />
+          {((curNode as FormGroup)?.parent as FormGroup)?.label || 
+          ((curNode as FieldNode)?.parent as FieldNode)?.title || 'Root Page'}
         </Link>)}
+        <Spacer transition="flex 0.2s" flex={page !== 'root' ? '1' : '0'} />
         <Button onClick={getFullStore}>PRINT DATA</Button>
     </Box>
-    {console.log({rootNode, curNode, page})}
-    { curNode ? (
-      <AnimatePresence>
-        {(page === 'root') && (<motion.div key={curNode.id}
-            style={pageStyle}
-            initial="initial"
-            animate="in"
-            exit="out"
-            variants={pageVariants}
-            transition={pageTransition}>
-            <Home rootNode={rootNode} curNode={curNode as Root} nextNode={nextNode} />
-          </motion.div>)}
-        {(page === 'group') && <motion.div key={curNode.id}
-            style={pageStyle}
-            initial="initial"
-            animate="in"
-            exit="out"
-            variants={pageVariants}
-            transition={pageTransition}>
-            <GroupPage
-                rootNode={rootNode} curNode={curNode as FormGroup} nextNode={nextNode} />
-          </motion.div>}
-          {(page === 'field') && <motion.div key={curNode.id}
+    <Box position="relative">
+      { curNode ? (
+        <AnimatePresence>
+          {(page === 'root') && (<motion.div key={curNode.id}
               style={pageStyle}
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={pageVariants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              variants={variants}
               transition={pageTransition}>
-              <FieldPage rootNode={rootNode} curNode={curNode as FieldNode} nextNode={nextNode}/>
+              <Home rootNode={rootNode} curNode={curNode as Root} nextNode={nextNode} />
+            </motion.div>)}
+          {(page === 'group') && <motion.div key={curNode.id}
+              style={pageStyle}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={pageTransition}>
+              <GroupPage
+                  rootNode={rootNode} curNode={curNode as FormGroup} nextNode={nextNode} />
             </motion.div>}
-      </AnimatePresence>) 
-      : <h1>Loading...</h1>
-    }
+            {(page === 'field') && <motion.div key={curNode.id}
+                style={pageStyle}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={pageTransition}>
+                <FieldPage rootNode={rootNode} curNode={curNode as FieldNode} nextNode={nextNode}/>
+              </motion.div>}
+        </AnimatePresence>) 
+        : <h1>Loading...</h1>
+      }
+    </Box>
+    
     
   </>)
 }
